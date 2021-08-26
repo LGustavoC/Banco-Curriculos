2ccbebe097cc2770d68a8667c57f0354119612b2
LGustavoC <gugacristo90@gmail.com>
2021-08-26 00:41:01 -0300

    Adicionado usuario controller model, função de adicionar usuario no mongodb

--- server.js
+++ server.js
@@ -5,12 +5,14 @@ const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const path = require('path');
 const mongoose = require('mongoose');
+const routes = require('./src/routes');
 
 const app = express();
 
 // Definindo porta, caso nao exista, recebe 5000
 const port = process.env.PORT || 5000
-const URI = 'mongodb://GugaUser:password@cluster0.33ujj.mongodb.net/banco-curriculos?retryWrites=true&w=majority';
+//const URI = 'mongodb://GugaUser:password@cluster0.33ujj.mongodb.net/banco-curriculos.usuarios?retryWrites=true&w=majority';
+const URI = 'mongodb://localhost:27017/banco-curriculos'
 
 // Conectando ao banco de dados (MongoDB)
 try {
@@ -18,7 +20,7 @@ try {
     console.log("MongoDB conectado com sucesso!"));    
     }catch (error) { 
     console.log("could not connect");    
-    }
+}
 /*mongoose.connect(URI, {
     useUnifiedTopology: true,
     useNewUrlParser: true,
@@ -38,9 +40,7 @@ app.use(cookieParser());
 // Receber e enviar JSON do front-end para o back-end 
 app.use(express.json());
 
-app.get('/', function(req, res) {
-    res.json({message: 'Hello world!'})
-});
+app.use(routes);
 
 app.listen(port, function() {
     console.log(`Server running at: ${port}`)
