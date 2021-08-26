c4e7747e3bef7ff1530ecce5b24c82c95d4a6bb1
LGustavoC <gugacristo90@gmail.com>
2021-08-25 23:03:38 -0300

    Ligando aplicação a base de dados na nuvem pelo MongoDB

--- server.js
+++ server.js
@@ -4,17 +4,37 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const path = require('path');
+const mongoose = require('mongoose');
 
 const app = express();
 
 // Definindo porta, caso nao exista, recebe 5000
 const port = process.env.PORT || 5000
+const URI = 'mongodb://GugaUser:password@cluster0.33ujj.mongodb.net/banco-curriculos?retryWrites=true&w=majority';
+
+// Conectando ao banco de dados (MongoDB)
+try {
+    mongoose.connect( URI, {useNewUrlParser: true, useUnifiedTopology: true}, () =>
+    console.log("MongoDB conectado com sucesso!"));    
+    }catch (error) { 
+    console.log("could not connect");    
+    }
+/*mongoose.connect(URI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useFindAndModify: false
+}, function (err){
+    if(err){
+        console.log(err);
+    }else {
+        console.log('MongoDB conectado com sucesso!')
+    }
+});*/
+
 
 // Quais dominios podem estar consumindo os dados da API
 app.use(cors());
-
 app.use(cookieParser());
-
 // Receber e enviar JSON do front-end para o back-end 
 app.use(express.json());
 
